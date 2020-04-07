1d28e048a33cd6da7f092b9b3f80dfc03f735277
huntmori04x22 <huntmori04x22@gmail.com>
2020-04-07 17:58:27 +0900

    Unity WebGL client test

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const http = require("http").createServer(app);
 const port = 8080;
 
+//app.use(express.static(__dirname+"/view"));
+app.use("/TemplateData", express.static(__dirname+"/TemplateData"));
+app.use("/Build", express.static(__dirname+"/Build"));
 app.get("/", function(request, response){
-    response.end("hello [express] world");
+    var data = fs.readFileSync(__dirname+"/index.html",'utf-8');
+    //console.log(data);
+    const ip = request.headers['x-forwarded-for'] ||  request.connection.remoteAddress;
+    console.log("access from : "+ip);
+    response.end(data);
 });
 
-app.use(express.static(__dirname+"/view"));
-const routes = require("./route/"); //index.js = / 
-app.use(routes);
+
+//const routes = require("./route/"); //index.js = / 
+//app.use(routes);
 
 http.listen(port, function(){
     console.log(`http://localhost:${port}`);
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import express, { application } from "express";
-
+//import express, { application } from "express"
+import express from "express";
+import fs from "fs";
 class App
 {
     public application : express.Application;
@@ -12,7 +13,10 @@ class App
 const app = new App().application;
 
 app.get("/", (req:express.Request, res:express.Response)=>{
-    res.send("start");
+    //res.send("start");
+    var data = fs.readFileSync("/inner_app/index.html",'utf-8');
+    console.log(data);
+    res.end(data);
 });
 
 app.listen(4000, ()=>console.log("start"));
