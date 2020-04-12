800737326a5449076a469e142ab705ac46322088
huntmori04x22 <huntmori04x22@gmail.com>
2020-04-12 17:46:33 +0900

    timestamp log

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const fs = require("fs");
 const app = express();
 const http = require("http").createServer(app);
 const port = 8080;
-
+const moment = require('moment');
+require('moment-timezone');
 //app.use(express.static(__dirname+"/view"));
+moment.tz.setDefault('Asia/Seoul');
+
 app.use("/TemplateData", express.static(__dirname+"/TemplateData"));
 app.use("/Build", express.static(__dirname+"/Build"));
 app.get("/", function(request, response){
     var data = fs.readFileSync(__dirname+"/index.html",'utf-8');
     //console.log(data);
     const ip = request.headers['x-forwarded-for'] ||  request.connection.remoteAddress;
-    console.log("access from : "+ip);
+    const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
+    console.log("["+timestamp+"] access from : "+ip);
     response.end(data);
 });
 
